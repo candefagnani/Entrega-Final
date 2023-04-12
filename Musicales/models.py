f1d49405de31cfa6eb17323fd69ad6a8b46a2a38
Candela Fagnani <cfagnani@itba.edu.ar>
2023-04-12 19:07:12 -0300

    Algunos cambios

--- Musicales/models.py
+++ Musicales/models.py
@@ -8,14 +8,14 @@ class Post(models.Model):
     leading_actor = models.CharField(max_length = 100)
     main_song = models.CharField(max_length = 100)
     song_duration = models.DecimalField(max_digits = 5, decimal_places = 2)
-    publisher = models.ForeignKey(to=User, on_delete = models.CASCADE, related_name = "publisher")
+    publisher = models.ForeignKey(to = User, on_delete = models.CASCADE, related_name = "publisher")
     imagen = models.ImageField(upload_to = "posts", null = True, blank = True)
     
     def __str__(self):
         return f"{self.id} - {self.name} ({self.year_of_opening})"
 
 class Profile(models.Model):
-    user = models.OneToOneField(to=User, on_delete = models.CASCADE, related_name = "profile")
+    user = models.OneToOneField(to= User, on_delete = models.CASCADE, related_name = "profile")
     instagram = models.CharField(max_length = 100)
     email = models.CharField(max_length = 100)
     imagen = models.ImageField(upload_to = "profiles", null = True, blank = True)
